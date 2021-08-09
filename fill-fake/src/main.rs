92fef9611cc6d147f015928b0e5db40d1bf98e68
grv07 <grvtyagi22@gmail.com>
2021-08-09 20:35:14 +0530

    will add walk later

--- fill-fake/src/main.rs
+++ fill-fake/src/main.rs
@@ -4,6 +4,7 @@ use std::path::Path;
 
 const BASE_PATH: &str = "/home/tyagig/obunch/migrations";
 
+// instead of creating the wheel again use WalkDir.
 fn get_all_files() -> Result<Vec<File>> {
     let migrations: Vec<_> = read_dir(Path::new(BASE_PATH))?
         .map(|res| {
